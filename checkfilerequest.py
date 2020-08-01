6f1297722f294af315354d65430847942a8f3acf
Raju <gujjalpatiraju@gmail.com>
2020-08-01 16:55:46 +0530

    Able to handle single requests

---
+++ checkfilerequest.py
@@ -0,0 +1,13 @@
+import requests
+import json
+from flask import request
+
+# url = "http://localhost:5000/api"  
+
+# data = {'firstname':"raju"}
+
+# headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+# r = requests.get(url,json=data, headers=headers)
+# print(r.url)
+# print(r.content)
--- matchingusers.py
+++ matchingusers.py
@@ -1,4 +1,4 @@
-from aadhar_pan_api import aadhar_check,pan_check, app
+from aadhar_pan_api import aadhar_check,pan_check, app, request
 from fuzzywuzzy import process,fuzz
 from datapreprocessing import Row_list
 import collections
@@ -7,22 +7,34 @@ import collections
 def error(e):
   return "Please Check Your Url and fill user details accordingly"
 
-@app.route('/api/userdata/<fname>/<lname>/<gender>/<cus_type>',methods=['GET'])
-def userdataMatching(fname,lname,gender,cus_type=None):
-    ret=fname,lname,gender,cus_type
-    user_input=str(','.join(ret))   
-    req_data = [ data[0:3]+data[5:6] for data in Row_list]
-    final_data=[ [','.join(x)] for x in req_data]
-    print(final_data)
-    f={}
-    for data in final_data:
-        results = fuzz.token_sort_ratio(user_input,data)
-        f[results]=data
-    res=sorted(f.items(),key=lambda x: x[0],reverse=True)
-    final_output = [f"The matching percentage is {str(i[0])} for input data {user_input} and compared data is {str(i[1])} '\n' " for i in res]
-    return str(final_output)
+def preprocess_user_data(ret):
+  user_input=str(','.join(ret))   
+  req_data = [ data[0:3]+data[5:6] for data in Row_list]
+  final_data=[ [','.join(x)] for x in req_data]
+  print(final_data)
+  f={}
+  for data in final_data:
+      results = fuzz.token_sort_ratio(user_input,data)
+      f[results]=data
+  res=sorted(f.items(),key=lambda x: x[0],reverse=True)
+  final_output = [f"The matching percentage is {str(i[0])} for input data {user_input} and compared data is {str(i[1])} '\n' " for i in res]
+  return str(final_output)
+
+# @app.route('/api',methods=['GET'])
+# def userdataMatching():
+#     fname = request.args.get('firstname')
+#     lname = request.args.get('lastname')
+#     gender = request.args.get('gender')
+#     cust_type = request.args.get('cust_type')
+#     dob = request.args.get('dob')
+
+    # if fname!=None and lname!=None and gender!=None and cust_type!=None:
+    #   ret=fname,lname,gender,cust_type
+    #   return preprocess_user_data(ret)
+      
+    
 
 
 
-# if __name__ == '__main__':
-#   app.run(debug=True)
+if __name__ == '__main__':
+  app.run(debug=True)
